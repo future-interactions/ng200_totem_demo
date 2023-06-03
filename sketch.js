9d8962d1fca9031cb4e364bde79f3f114f3b6db5
Matt Wade <matt@Matts-MBP.fritz.box>
2023-06-03 20:42:07 +0100

    centering text

--- sketch.js
+++ sketch.js
@@ -2,12 +2,14 @@ var vSize = 10;
 var stepper = 100;
 let bgImages = [];
 let words = [];
+let myFont;
 var scalerPerc=.33;
 function preload() {
   bgImages[0] = loadImage('assets/whistlejacket.png');
   bgImages[1] = loadImage('assets/whistlejacket.png');
   bgImages[2] = loadImage('assets/sunflowers.png');
   bgImages[3] = loadImage('assets/duchess.png');
+  myFont = loadFont('assets/DMSans-Medium.ttf');
 
 }
 
@@ -20,6 +22,11 @@ function setup() {
 	words[1] = 'whistlejacket';
 	words[2] = 'sunflowers';
 	words[3] = 'duchess';
+	textFont(myFont);
+	textSize(32);
+fill(255);
+textAlign(CENTER, BASELINE);
+
 }
 
 function draw() {
@@ -28,8 +35,7 @@ function draw() {
 function drawContent() {
 	background(0);
 	image(bgImages[key], windowWidth/2-(bgImages[0].width *scalerPerc)/2,windowHeight/2-(bgImages[0].height*scalerPerc) / 2,(bgImages[0].width *scalerPerc),(bgImages[0].height *scalerPerc));
-	textSize(32);
-	text(words[key], windowWidth/2-(bgImages[0].width *scalerPerc)/2,windowHeight/2-(bgImages[0].height*scalerPerc) / 2);
+	text(words[key], windowWidth/2,windowHeight/2);
 }
 function keyTyped() {
 	drawContent(key);
